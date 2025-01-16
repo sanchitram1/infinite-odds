adacaca6b5f777e69c122ff8f72251f0d55ac9a7
Sanchit Ram Arvind <sanchitram@gmail.com>
2025-01-16 17:18:08 -0600

    hardhat and package.json

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    teatestnet: {
+      url: `https://71947.rpc.thirdweb.com/${process.env.THIRDWEB_API_KEY}`,
+      chainId: 71947,
+    },
+  },
 };
