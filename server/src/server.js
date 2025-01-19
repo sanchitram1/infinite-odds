022de94068d2f1fd59680b395a5faa01c49630ed
Sanchit Ram Arvind <sanchitram@gmail.com>
2025-01-20 00:57:52 +0530

    -m
     closes #no
    
     closes #auth

--- server/src/server.js
+++ server/src/server.js
@@ -12,8 +12,7 @@ export default async function (fastify, opts) {
   // Register CORS
   await fastify.register(cors, {
     origin: (process.env.CORS_ORIGIN || 'http://localhost:3000').split(','),
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
   });
 
   // Register routes
